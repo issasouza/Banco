87d9b965478461b142bf8a5112bb315582eb2dc4
isabelly souza <isabellysouza1998@gmail.com>
2024-08-02 16:46:50 -0300

    first commit

---
+++ src/cliente/entities/cliente.entity.ts
@@ -0,0 +1,13 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+Entity()
+export class Cliente {
+    @PrimaryGeneratedColumn()
+    
+}
+/*-nomeCompleto: String
+-id: String
+-endereco: String
+-telefone: String
+-Conta: ContaBancaria
+-gerente: Gerente*/
